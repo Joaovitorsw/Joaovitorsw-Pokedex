22687ce2c81328da4c37d0cff16b9c36bab59ac2
Joaovitorsw <joaovitorwbr@gmail.com>
2021-10-02 05:25:06 -0300

    ✨ feat: home ui create pokemon card

--- src/classes/home-ui.js
+++ src/classes/home-ui.js
@@ -12,4 +12,39 @@ export class HomeUI {
       $pokemonsContent.append($pokeCard);
     }
   }
+
+  static async createPokemonCard(id) {
+    const pokemon = await PokeAPI.getPokemon(id);
+    const { name: pokemonName } = pokemon;
+
+    const maskedPokemonID = Utils.getMaskedID(pokemon);
+    const firstTypeName = Utils.getFirstType(pokemon);
+
+    const spriteVersionUrl = Utils.getPokemonSpriteUrl(pokemon);
+
+    const types = Utils.getPokemonTypes(pokemon);
+
+    const $routerLink = document.createElement("a");
+
+    const $pokeTypes = Utils.createElementWithClass("div", "poke-types");
+    const $pokeCard = Utils.createElementWithClass("div", firstTypeName);
+    const $pokeInfo = Utils.createElementWithClass("div", "poke-info");
+
+    $pokeTypes.innerText = "Types:";
+
+    types.forEach((pokeTypes) => Utils.appendPokemonType(pokeTypes, $pokeTypes));
+
+    $routerLink.href = `#details/${pokemonName}`;
+    $pokeInfo.innerHTML = `  
+        <img src="${spriteVersionUrl}" alt="${pokemonName}" />
+        <h1 class="poke-number">#${maskedPokemonID}</h1>
+        <h2 class="poke-name">${pokemonName}</h2>  
+  `;
+
+    $pokeCard.appendChild($pokeInfo);
+    $pokeCard.appendChild($pokeTypes);
+    $routerLink.append($pokeCard);
+
+    return $routerLink;
+  }
 }
