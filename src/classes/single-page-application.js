04dda3a2f2d73acdfe40af5bce6f43d832b1d9df
Joaovitorsw <joaovitorwbr@gmail.com>
2021-10-02 05:16:54 -0300

    ✨ feat: spa get target route

--- src/classes/single-page-application.js
+++ src/classes/single-page-application.js
@@ -5,4 +5,9 @@ export class SinglePageApplication {
   static addHashListener() {
     window.addEventListener("hashchange", SinglePageApplication.renderPage);
   }
+
+  static getTargetRoute(hash) {
+    const hashIsEmpty = hash === "";
+    return hashIsEmpty ? "home" : hash.replace("#", "");
+  }
 }
