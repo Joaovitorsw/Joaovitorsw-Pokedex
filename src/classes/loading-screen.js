1953da6bc7d5e7fc4ab2bb7e7ec121f5b1ae18f8
Joaovitorsw <joaovitorwbr@gmail.com>
2021-10-03 15:25:37 -0300

    💄 style: loading screen prettier

--- src/classes/loading-screen.js
+++ src/classes/loading-screen.js
@@ -3,9 +3,10 @@ import { Utils } from "./utils.js";
 export class LoadingScreen {
   static createContent() {
     const $loadingScreen = Utils.createElementWithClass("div", "loader");
-    $loadingScreen.innerHTML = ` <div class="pokeball">
+    $loadingScreen.innerHTML = ` 
+    <div class="pokeball">
     <div class="pokeball-button"></div>
-  </div>
+    </div>
   `;
     return $loadingScreen;
   }
