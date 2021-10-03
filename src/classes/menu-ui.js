77f1b1a0032a4ad44881e20f6792d0e33b1745b1
Joaovitorsw <joaovitorwbr@gmail.com>
2021-10-03 15:25:15 -0300

    ✨ feat: menu ui class

---
+++ src/classes/menu-ui.js
@@ -0,0 +1,40 @@
+import { BasicStorage } from "./basic-storage.js";
+import { FloatingLabelUI } from "./floating-label-ui.js";
+import { HomeUI } from "./home-ui.js";
+
+export class MenuUI {
+  static $selectBox;
+  static $selectItem;
+  static $optionsContainer;
+  static $menuList;
+  static pokemonsArray;
+
+  static menuUI() {
+    MenuUI.$selectBox = document.querySelector(".select-box");
+    MenuUI.$selectItem = MenuUI.$selectBox.querySelector(".selected h1");
+    MenuUI.$menuList = MenuUI.$selectBox.querySelectorAll(".option");
+    MenuUI.$optionsContainer = MenuUI.$selectBox.querySelector(".options-container");
+
+    MenuUI.menuContentListener();
+    MenuUI.menuAddListener();
+  }
+
+  static menuContentListener() {
+    MenuUI.$selectBox.addEventListener("click", () => MenuUI.$optionsContainer.classList.toggle("active"));
+  }
+
+  static menuAddListener() {
+    MenuUI.$menuList.forEach(($itemMenu) => {
+      $itemMenu.addEventListener("click", () => {
+        const generation = $itemMenu.querySelector("label").innerText;
+        const generationRange = `${$itemMenu.querySelector("input").value}`.split(":");
+        const generations = { start: parseFloat(generationRange[0]), end: parseFloat(generationRange[1]) };
+        HomeUI.maxPokemons = generations.end;
+        HomeUI.pokemonArrays = BasicStorage.get("pokemons").slice(generations.start, HomeUI.maxPokemons);
+        FloatingLabelUI.updatePokemon();
+        HomeUI.searchIsEmpty = false;
+        MenuUI.$selectItem.innerText = generation;
+      });
+    });
+  }
+}
