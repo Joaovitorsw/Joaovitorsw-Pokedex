dc6664545c4628963606b756dbf1327377fda22f
Joaovitorsw <joaovitorwbr@gmail.com>
2021-10-03 02:56:15 -0300

    ✨ feat: homepage navigation content

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,6 +1,14 @@
 import { Utils } from "../classes/utils.js";
 
 export async function HomePage() {
+  const $homepage = Utils.createElementWithClass("div", "homepage");
+  $homepage.innerHTML = `  <div class="navigation-content">
+          <div class="search-bar">
+            <img class="search-bar-icon" alt="search-bar-icon" />
+            <input class="search-bar-input" type="text" />
+            <label class="search-bar-label"">Search for pokemon...</label>
+          </div>`;
   const $pokemonsContent = Utils.createElementWithClass("div", "pokemons-content");
-  return $pokemonsContent;
+  $homepage.append($pokemonsContent);
+  return $homepage;
 }
