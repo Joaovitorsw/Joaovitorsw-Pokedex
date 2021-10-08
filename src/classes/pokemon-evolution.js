be6bfbf70d02d7297f9a7c93cee694b4ef0eed8e
Joaovitorsw <joaovitorswbr@gmail.com>
2021-10-08 08:38:20 -0300

    ✨ feat: pokemon evolution add eevee all forms

--- src/classes/pokemon-evolution.js
+++ src/classes/pokemon-evolution.js
@@ -7,6 +7,8 @@ export class PokemonEvolution {
     const $evolution = Utils.createElementWithClass("div", "evolution");
     const pokemon = data;
     const pokemonEvolutionMap = await PokeAPI.getEvolutionChain(pokemon.pokemonID);
+    const EeveeEvolutions = pokemonEvolutionMap.id === 67;
+
     const firstForm = pokemonEvolutionMap.chain.species.name;
     const secondForm = pokemonEvolutionMap.chain.evolves_to[0]?.species.name;
     const thirdForm = pokemonEvolutionMap.chain.evolves_to[0]?.evolves_to[0]?.species.name;
@@ -20,8 +22,7 @@ export class PokemonEvolution {
             </a>
              <span>Evolves to </span>
              <a href="#details/${secondForm}">
-            <img src="${Utils.getPokeImageUrl(secondForm)}" alt="${secondForm}" data-name="${secondForm}"></a>
-            
+            <img src="${Utils.getPokeImageUrl(secondForm)}" alt="${secondForm}" data-name="${secondForm}"></a>          
   `;
     }
 
@@ -38,6 +39,23 @@ export class PokemonEvolution {
             </div>`;
     }
 
+    if (EeveeEvolutions) {
+      $evolution.innerHTML = `<h1 class="evolution-title">Evolution Chain</h1>`;
+      pokemonEvolutionMap.chain.evolves_to.forEach((evolution) => {
+        $evolution.innerHTML += `
+            <div class="first-form">
+             <a href="#details/${firstForm}">
+            <img src="${Utils.getPokeImageUrl(firstForm)}" alt="${firstForm}-image" data-name="${firstForm}">
+            </a>
+             <span>Evolves to </span>
+             <a href="#details/${evolution.species.name}">
+            <img src="${Utils.getPokeImageUrl(evolution.species.name)}"
+             alt="${evolution.species.name}" 
+             data-name="${evolution.species.name}">
+            </a>
+            `;
+      });
+    }
     $slide.append($evolution);
 
     return $slide;
