f677230708fb913244c17dad2490d5c483b2123a
Joaovitorsw <joaovitorswbr@gmail.com>
2021-10-11 20:29:52 -0300

    ✨ feat: login screen methods loginscreen ui and create login screen

--- src/classes/login-screen.js
+++ src/classes/login-screen.js
@@ -18,4 +18,48 @@ export class LoginScreen {
   userIsValid = false;
   $password;
   $html = document.documentElement;
+
+  show() {
+    this.#loginScreenUI();
+  }
+
+  #loginScreenUI() {
+    const $loginScreen = this.#createLoginScreen();
+    $main.append($loginScreen);
+    this.$html.classList.add("login");
+    this.#formSelector();
+  }
+
+  #createLoginScreen() {
+    const $loginScreen = Utils.createElementWithClass("div", "login-screen");
+    $loginScreen.innerHTML = `
+    <button class="exit">X</button>
+     <div class="form">
+        <div class="thumbnail">
+            <img>
+        </div>
+        <form class="register-form">
+            <input type="text" placeholder="Email address"/>
+            <span class="form-error">Invalid email address</span>
+            <input type="password" placeholder="Password"/>
+            <span class="form-error">Invalid email address</span>
+            <button disabled>Create</button>
+            <p class="message">Already registered? 
+            <a>Sign In</a>
+            </p>
+        </form>
+        <form class="login-form">
+            <input type="text" placeholder="Email address"/>
+            <span class="form-error">Invalid email address</span>
+            <input type="password" placeholder="Password"/>
+            <span class="form-error">Invalid email address</span>
+            <button disabled>Login</button>
+            <p class="message">Not registered? 
+            <a>Create an account</a>
+            </p>
+        </form>
+     </div>
+          `;
+    return $loginScreen;
+  }
 }
