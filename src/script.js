7adf69c3deb4e8e2ef2a87c6aa2bcabec344364f
Joaovitorsw <joaovitorswbr@gmail.com>
2021-10-11 20:25:15 -0300

    ✨ feat: script fire base start

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
+import { FireBase } from "./classes/fire-base.js";
 import { SinglePageApplication } from "./classes/single-page-application.js";
 
 export const $main = document.querySelector("#root");
-
 SinglePageApplication.addHashListener();
 SinglePageApplication.addWindowLoadListener();
+FireBase.start();
