e29852e35fad44a8c1bece71977a2b4acbdf9c0e
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-11-17 07:31:15 -0300

    ✨ feat: observable class

---
+++ src/classes/observable.js
@@ -0,0 +1,19 @@
+export class Observable {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(observerFunction) {
+    this.observers.push(observerFunction);
+  }
+
+  unsubscribe(observerFunction) {
+    this.observers = this.observers.filter((subscriber) => subscriber !== observerFunction);
+  }
+
+  publish(data) {
+    this.observers.forEach((observer) => observer(data));
+
+    console.log(`Notifying ${this.observers.length} Observers`, data);
+  }
+}
