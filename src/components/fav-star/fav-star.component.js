80e3c9f91983ae22122167ecfcb43078eed4e368
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-11-18 20:52:43 -0300

    ✨ feat: fav observable

--- src/components/fav-star/fav-star.component.js
+++ src/components/fav-star/fav-star.component.js
@@ -1,18 +1,31 @@
+import { Observable } from "../../classes/observable";
+import { FireBaseService } from "../../services/fire-base.service";
+import { UtilsService } from "../../services/utils.service";
 import favStarTemplate from "./fav-star.component.html";
 import favStarStyle from "./fav-star.component.scss";
 
 export class FavStarComponent extends HTMLElement {
+  #fireBaseService;
   constructor() {
     super();
+    this.observable$ = new Observable();
+    this.#fireBaseService = new FireBaseService();
   }
   connectedCallback() {
     const style = favStarStyle;
     this.innerHTML = favStarTemplate;
     this.$favContent = this.querySelector(".fav-content");
-    this.addEventListener("click", this.toggleStar);
+    this.addEventListener("click", () => {
+      if (this.#fireBaseService.getUser()) {
+        this.toggleStar();
+        return;
+      }
+      UtilsService.notificationAlert("error", "You must be logged");
+    });
   }
-  toggleStar() {
-    this.$favContent.classList.toggle("active");
+  async toggleStar() {
+    const boolean = this.$favContent.classList.toggle("active");
+    this.observable$.publish(boolean);
   }
 }
 customElements.define("fav-star", FavStarComponent);
--- src/components/poke-card/poke-card.component.js
+++ src/components/poke-card/poke-card.component.js
@@ -1,15 +1,21 @@
+import { FireBaseService } from "../../services/fire-base.service.js";
 import { UtilsService } from "../../services/utils.service.js";
 import { FavStarComponent } from "../fav-star/fav-star.component.js";
 import pokemonCardTemplate from "./poke-card.component.html";
 import pokemonCardStyle from "./poke-card.component.scss";
 
 export class PokemonCardComponent extends HTMLElement {
+  #fireBaseService;
+  constructor() {
+    super();
+    this.#fireBaseService = new FireBaseService();
+    this.declarations = [FavStarComponent];
+  }
   connectedCallback() {
     const style = pokemonCardStyle;
 
     const pokemonJSON = this.attributes.pokemon.value;
     this.removeAttribute("pokemon");
-
     const pokemon = JSON.parse(pokemonJSON);
     const [type1, type2] = UtilsService.getPokemonTypes(pokemon);
     const maskedID = UtilsService.getMaskedID(pokemon);
@@ -23,10 +29,14 @@ export class PokemonCardComponent extends HTMLElement {
       type1,
       type2,
     };
-
     const template = UtilsService.bindModelToView(pokemonModel, pokemonCardTemplate);
+    this.pokemonID = pokemon.id;
     this.innerHTML = template;
-    FavStarComponent;
+    const favStar = this.querySelector("fav-star");
+    favStar.observable$.subscribe(async (boolean) => {
+      if (boolean) await this.#fireBaseService.addFavoritePokemon(pokemon);
+      else await this.#fireBaseService.removeFavoritePokemon(pokemon);
+    });
   }
 }
 
