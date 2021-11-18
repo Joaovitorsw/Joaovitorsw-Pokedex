038fe525d46627e0bd0c0d0c3fd623883e4c22da
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-11-18 20:51:59 -0300

    ✨ feat: add settings page

--- src/constants/ROUTES.js
+++ src/constants/ROUTES.js
@@ -1,7 +1,9 @@
+import { DetailsPage } from "../pages/details.page.js";
 import { HomePage } from "../pages/home.page.js";
 import { SettingsPage } from "../pages/settings.page.js";
 
 export const ROUTES = {
-  home: new HomePage(),
-  settings: new SettingsPage(),
+  home: HomePage,
+  details: DetailsPage,
+  settings: SettingsPage,
 };
---
+++ src/pages/settings.page.js
@@ -0,0 +1,11 @@
+import { UserSettingsComponent } from "../components/user-settings/user-settings.component";
+
+export class SettingsPage {
+  constructor() {
+    this.declarations = [UserSettingsComponent];
+  }
+  async getTemplate() {
+    const $settingsPage = document.createElement("user-settings");
+    return $settingsPage;
+  }
+}
