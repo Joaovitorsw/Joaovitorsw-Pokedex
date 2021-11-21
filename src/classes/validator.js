57645ce0d8d9042ba331f997423c04245e637480
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-11-21 15:46:33 -0300

    🚧 chore: remove console log

--- src/classes/validator.js
+++ src/classes/validator.js
@@ -25,40 +25,6 @@ export class Validators {
   }
 
   isValidAllProperties() {
-    console.log(this.name, this.email, this.password, this.passwordMatch);
-    console.log(this.email && this.name && this.password && this.passwordMatch);
     return this.email && this.name && this.password && this.passwordMatch;
   }
-
-  // #regexIsValid($input, pattern, property) {
-  //   const namePattern = /[A-Z][a-z]* [A-Z][a-z]*/;
-
-  //   const passwordPattern = /^[a-zA-Z0-9!@#$%^&*]{9,16}$/;
-  //   const validate = {
-  //     "#name": () => (this.name = isValid),
-  //     "#email": () => (this.email = isValid),
-  //     "#password": () => (this.password = isValid),
-  //   };
-  //   $input.classList.add("invalid");
-  //   const isValid = pattern.test($input.value);
-  //   if (isValid) {
-  //     $input.classList.remove("invalid");
-  //   }
-  //   validate[property]();
-  //   this.buttonEnable;
-  // }
-
-  // #passwordIsValid($input, $inputConfirmPassword, property) {
-  //   const validate = {
-  //     "#passwordMatch": () => (this.passwordMatch = isValid),
-  //   };
-  //   $input.classList.add("invalid");
-  //   const isValid = $input.value === $inputConfirmPassword.value;
-  //   if (isValid) {
-  //     $input.classList.remove("invalid");
-  //   }
-
-  //   validate[property]();
-  //   this.buttonEnable;
-  // }
 }
